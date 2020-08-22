3f56a31dbec1f14ba8fc7948677ae6725b958022
Austin Wright <aaa@bzfx.net>
2020-08-22 11:24:12 -0700

    Update for GitHub Pages

--- gh-publish.sh
+++ gh-publish.sh
@@ -3,14 +3,14 @@ set -x
 BUILDDIR=.gh-pages-build
 TARGET_BRANCH=gh-pages
 # Check out the current `master` branch in a new directory
-test -f doc/index.xhtml || exit 1
+test -f docs/index.xhtml || exit 1
 git worktree add --detach $BUILDDIR master
 pushd $BUILDDIR
-# Build things (runs `make` in the doc/ directory)
-make DOC_TARGET=doc BROWSERIFY=../node_modules/.bin/browserify CODEMIRROR=../node_modules/codemirror
+# Build things (runs `make` in the docs/ directory)
+make DOC_TARGET=docs BROWSERIFY=../node_modules/.bin/browserify CODEMIRROR=../node_modules/codemirror
 # Add and commit the new tree to the $TARGET_BRANCH branch
-git add -f doc/*
-BUILD_ID=$(git commit-tree -p master -m 'gh-pages build' $(git write-tree --prefix=doc/))
+git add -f docs/*
+BUILD_ID=$(git commit-tree -p master -m 'gh-pages build' $(git write-tree))
 popd
 # Push that branch to the gh-publish remote
 git worktree remove -f $BUILDDIR
