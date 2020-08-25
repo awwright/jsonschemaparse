9443fbfb2c4c086a1653fffa398e34f1d96cca89
Austin Wright <aaa@bzfx.net>
2020-08-25 14:41:41 -0700

    docs: Lint the schema too

--- docs/app.src.js
+++ docs/app.src.js
@@ -1,10 +1,78 @@
 "use strict";
 var CodeMirror = window.CodeMirror = require('codemirror');
 var JSONSchemaParse = window.JSONSchemaParse = require('..');
+const metaschemas = [
+	require('json-metaschema/draft-2019-09-schema.json'),
+	require('json-metaschema/draft-2019-09-hyper-schema.json'),
+	require('json-metaschema/draft-2019-09-meta-applicator.json'),
+	require('json-metaschema/draft-2019-09-meta-content.json'),
+	require('json-metaschema/draft-2019-09-meta-core.json'),
+	require('json-metaschema/draft-2019-09-meta-format.json'),
+	require('json-metaschema/draft-2019-09-meta-meta-data.json'),
+	require('json-metaschema/draft-2019-09-meta-validation.json'),
+	require('json-metaschema/draft-2019-09-schema.json'),
+];
 var modes = [
 	require('codemirror/mode/javascript/javascript.js'),
 	require('codemirror/addon/lint/lint.js'),
 ];
+
+function parseLint(text, schema){
+	var found = [];
+	try {
+		var parse = JSONSchemaParse.parseInfo(text, {parseAnnotations:true, parseInfo:true, schema:schema});
+	} catch(err) {
+		var loc = err.position || {};
+		found.push({
+			from: CodeMirror.Pos(loc.line, loc.column-1),
+			to: CodeMirror.Pos(loc.line, loc.column),
+			message: err.message,
+			severity: 'error',
+		});
+	}
+	if(parse && parse.errors) parse.errors.forEach(function(err){
+		var layer = err.layer || {};
+		found.push({
+			from: CodeMirror.Pos(layer.beginLine, layer.beginColumn),
+			to: CodeMirror.Pos(layer.endLine, layer.endColumn),
+			message: err.message,
+			severity: 'warning',
+		});
+	});
+	if(parse && parse.annotations) parse.annotations.forEach(function(ann){
+		var layer = ann.layer || {};
+		found.push({
+			from: CodeMirror.Pos(layer.beginLine, layer.beginColumn),
+			to: CodeMirror.Pos(layer.beginLine, layer.beginColumn+1),
+			message: ann.value,
+			severity: 'message',
+		});
+	});
+	return found;
+
+}
+
+window.JSONSchemaLint = function JSONSchemaLint(text){
+	console.log(text);
+	if (!window.JSONSchemaParse) {
+		if (window.console) {
+			window.console.error("Error: window.JSONSchemaParse not defined, CodeMirror JSON linting cannot run.");
+		}
+		return [];
+	}
+
+	var registry = new JSONSchemaParse.SchemaRegistry;
+	// This passes just one test
+	metaschemas.forEach(function(v){ registry.scan(v); });
+	const schema = registry.lookup('https://json-schema.org/draft/2019-09/schema');
+	if(!schema){
+		if (window.console) {
+			window.console.error("Error: Schema <https://json-schema.org/draft/2019-09/schema> not found");
+		}
+		return [];
+	}
+	return parseLint(text, schema);
+};
 
 // declare global: JSONLint
 window.JSONLint = function JSONLint(schema_editor, text) {
@@ -27,35 +95,5 @@ window.JSONLint = function JSONLint(schema_editor, text) {
 			var schema = new JSONSchemaParse.Schema('_:root', schemaObject);
 		}
 	}
-	try {
-		const parse = JSONSchemaParse.parseInfo(text, {parseAnnotations:true, parseInfo:true, schema:schema});
-		console.log(schema, parse);
-		if(parse && parse.errors) parse.errors.forEach(function(err){
-			var layer = err.layer || {};
-			found.push({
-				from: CodeMirror.Pos(layer.beginLine, layer.beginColumn),
-				to: CodeMirror.Pos(layer.endLine, layer.endColumn),
-				message: err.message,
-				severity: 'warning',
-			});
-		});
-		if(parse && parse.annotations) parse.annotations.forEach(function(ann){
-			var layer = ann.layer || {};
-			found.push({
-				from: CodeMirror.Pos(layer.beginLine, layer.beginColumn),
-				to: CodeMirror.Pos(layer.beginLine, layer.beginColumn+1),
-				message: ann.value,
-				severity: 'message',
-			});
-		});
-	} catch(err) {
-		var loc = err.position || {};
-		found.push({
-			from: CodeMirror.Pos(loc.line, loc.column-1),
-			to: CodeMirror.Pos(loc.line, loc.column),
-			message: err.message,
-			severity: 'error',
-		});
-	}
-	return found;
+	return parseLint(text, schema);
 };
