3951edc328768f64616593f947a14b6b229306e0
Austin Wright <aaa@bzfx.net>
2020-09-08 20:23:28 -0700

    Test for expected module interface in interface.test.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports.StreamParser = require('./lib/parse.js').StreamParser;
 module.exports.parse = require('./lib/parse.js').parse;
 module.exports.parseInfo = require('./lib/parse.js').parseInfo;
-module.exports.SyntaxError = require('./lib/parse.js').SyntaxError;
-module.exports.ValidationError = require('./lib/schema.js').ValidationError;
+module.exports.SyntaxError = require('./lib/error.js').SyntaxError;
+module.exports.ValidationError = require('./lib/error.js').ValidationError;
 module.exports.SchemaRegistry = require('./lib/schema.js').SchemaRegistry;
 module.exports.Schema = require('./lib/schema.js').Schema;
--- test/interface.test.js
+++ test/interface.test.js
@@ -18,6 +18,30 @@ const metaschemas = [
 	require('json-metaschema/draft-2019-09-schema.json'),
 ];
 
+describe('interface', function(){
+	it('StreamParser', function(){
+		assert(typeof lib.StreamParser === 'function');
+	});
+	it('parse', function(){
+		assert(typeof lib.parse === 'function');
+	});
+	it('parseInfo', function(){
+		assert(typeof lib.parseInfo === 'function');
+	});
+	it('SyntaxError', function(){
+		assert(typeof lib.SyntaxError === 'function');
+	});
+	it('ValidationError', function(){
+		assert(typeof lib.ValidationError === 'function');
+	});
+	it('SchemaRegistry', function(){
+		assert(typeof lib.SchemaRegistry === 'function');
+	});
+	it('Schema', function(){
+		assert(typeof lib.Schema === 'function');
+	});
+});
+
 describe('parse(text)', function(){
 	it('parse valid', function(){
 		const ret = lib.parse('true');
