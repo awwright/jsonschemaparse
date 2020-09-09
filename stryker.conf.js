fe2fbbe58030a8df5521ddaf2d976679f285a0e1
Austin Wright <aaa@bzfx.net>
2020-09-08 23:12:25 -0700

    Add Stryker for testing

---
+++ stryker.conf.js
@@ -0,0 +1,28 @@
+"use strict";
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+	mutator: "javascript",
+	packageManager: "yarn",
+	reporters: ["html", "clear-text", "progress"],
+	testRunner: "mocha",
+	transpilers: [],
+	testFramework: "mocha",
+	coverageAnalysis: "perTest",
+	mochaOptions: {
+		// Optional mocha options
+		spec: [ 'test/interface.test.js', 'test/parse.test.js', 'test/parseInfo.test.js', 'test/validate.test.js', 'test/syntax-suite.test.js' ],
+	},
+	mutate: [
+		'index.js',
+		'lib/*.js',
+	],
+	files: [
+		'index.js',
+		'lib/*.js',
+		'test/*.test.js',
+		'test/vendor-syntax-suite/test_parsing/*.json',
+		'test/vendor-schema-suite/tests/**/*.json',
+	],
+};
